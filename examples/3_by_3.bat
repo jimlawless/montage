1df77601b16783e59b7666a630078ae664d63de2
Jim Lawless <lawless.jim@gmail.com>
2025-02-02 18:39:51 -0600

    Initial upload of example graphics.
    Added example graphics files and sample command options.

---
+++ examples/3_by_3.bat
@@ -0,0 +1,2 @@
+@echo off
+python montage.py -listfile covers.txt -rows 3 -cols 3 -xmax 150 -ymax 225 -imgfile g.jpg -bgcolor black
---
+++ examples/4_by_2.bat
@@ -0,0 +1,2 @@
+@echo off
+python montage.py -listfile covers.txt -rows 2 -cols 4 -xmax 150 -ymax 225 -imgfile g.jpg -bgcolor black
